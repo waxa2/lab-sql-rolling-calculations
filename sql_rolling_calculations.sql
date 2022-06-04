40dd4b2e971b507cf79eed1a9b4a374b29506107
Xabier Laibarra <72040528+waxa2@users.noreply.github.com>
2022-06-04 15:52:18 +0200

    sql rolling calculations

--- sql_rolling_calculations.sql
+++ sql_rolling_calculations.sql
@@ -31,9 +31,5 @@ GROUP BY previous_month_cust, month
 HAVING month= previous_month_cust+1;
 
 
-SELECT customer_id, LEFT(return_date,4) as year, MONTH(return_date) as month,LAG(MONTH(return_date),1) OVER (PARTITION BY customer_id) as previous_month_cust
-FROM rental
-GROUP BY customer_id, year,month
-ORDER BY customer_id ASC, year ASC, month ASC
 
 
